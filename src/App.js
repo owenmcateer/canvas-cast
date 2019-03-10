d49f03b1f8047a37d8d57c21c952d023219f3257
Owen McAteer <o.mcateer@catchdigital.com>
2019-03-10 22:58:04 +0100

    Added functionality for custom matrix layouts.

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ class CanvasCast {
     this.height = settings.height;
     this.brightness = settings.brightness;
     this.type = settings.type;
+    this.customMap = settings.customMap || false;
+
     this.stageResized = false;
     this.wsOpen = false;
     this.ws = false;
@@ -75,6 +77,7 @@ class CanvasCast {
 
   /**
    * Status bar
+   * @param {Int} statusId Statis ID number
    */
   status(statusId) {
     const open = this.ws;
@@ -150,11 +153,15 @@ class CanvasCast {
 
   /**
    * Matrix Serpentine Layout
+   *
    * If your matrix is in a zigzag serpentine layout
    * this code will order the data array being sent
    * to the matrix.
+   *
+   * @param  {Int} p Pixel position in canvas
+   * @return {Int} Position in Matrix 
    */
-  matrixMapping(p) {
+  matrixSerpentine(p) {
     let position;
 
     // Odd rows get reversed
@@ -210,8 +217,8 @@ class CanvasCast {
     }
 
     // Create 8 bit array buffer
-    const buffer = new ArrayBuffer(this.width * this.height * 3);
-    const bytearray = new Uint8Array(buffer, 0, this.width * this.height * 3);
+    const buffer = new ArrayBuffer(this.getBufferSize());
+    const bytearray = new Uint8Array(buffer, 0, this.getBufferSize());
 
     // 2D or WebGL
     let context;
@@ -226,30 +233,88 @@ class CanvasCast {
     }
 
     // Build and order data array
-    for (let i = 0; i < (this.width * this.height * 3); i += 3) {
+    for (let i = 0; i < (this.getBufferSize()); i += 3) {
       const p = i / 3;
-      const y = Math.floor(p / this.width);
-      const x = p - (y * this.width);
-      const arrayPos = this.matrixMapping(p);
+
+      // Get canvas mapping info
+      const canvasData = this.canvasMapping(p);
       let pixel;
 
       // Get pixel data
       if (this.type === '2d') {
-        pixel = context.getImageData(x, y, 1, 1).data;
+        pixel = context.getImageData(canvasData.x, canvasData.y, 1, 1).data;
       }
       else if (this.type === 'webgl') {
         pixel = new Uint8Array(4);
-        context.readPixels(x, y, 1, 1, context.RGBA, context.UNSIGNED_BYTE, pixel);
+        context.readPixels(canvasData.x, canvasData.y, 1, 1, context.RGBA, context.UNSIGNED_BYTE, pixel);
       }
 
       // Add pixel data to array
-      bytearray[arrayPos] = pixel[0];
-      bytearray[arrayPos + 1] = pixel[1];
-      bytearray[arrayPos + 2] = pixel[2];
+      bytearray[canvasData.position] = pixel[0];
+      bytearray[canvasData.position + 1] = pixel[1];
+      bytearray[canvasData.position + 2] = pixel[2];
     }
 
     // Send data!
     this.ws.send(bytearray);
+  }
+
+  /**
+   * Render pixel guidelines for custom matrix
+   * @param {DOM Canvas} canvas DOM Canvas
+   * @param {Int} size Pixel size of guide pixel
+   */
+  guide(canvas, size) {
+    const ctx = canvas.getContext('2d');
+
+    // Draw the pixel map guide
+    for (var i = 0; i < this.customMap.length; i++) {
+      ctx.strokeStyle = 'black';
+      ctx.beginPath();
+      ctx.ellipse(this.customMap[i].x, this.customMap[i].y, size, size, Math.PI / 4, 0, 2 * Math.PI);
+      ctx.stroke();
+
+      ctx.strokeStyle = 'white';
+      ctx.beginPath();
+      ctx.ellipse(this.customMap[i].x, this.customMap[i].y, size - 1, size - 1, Math.PI / 4, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+  }
+
+  /**
+   * Returns require graphic buffer size
+   * @return {Int} Graphic buffer size
+   */
+  getBufferSize() {
+    if (this.customMap) {
+      return this.customMap.length * 3;
+    }
+
+    // Else return default matrix
+    return this.width * this.height * 3
+  }
+
+  /**
+   * Maps canvas position to Matrix position
+   * @return {Obj} {x, y, position}
+   */
+  canvasMapping(pixel) {
+    // Custom pixel mapping
+    if (this.customMap) {
+      return {
+        y: this.customMap[pixel].y,
+        x: this.customMap[pixel].x,
+        position: pixel * 3,
+      };
+    }
+
+    // Standard matrix
+    const y = Math.floor(pixel / this.width);
+    return {
+      y,
+      x: pixel - (y * this.width),
+      position: this.matrixSerpentine(pixel),
+    };
   }
 }
 
